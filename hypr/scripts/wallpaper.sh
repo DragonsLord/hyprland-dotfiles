cc5bef357cfca71bf0a4ae463149752315ce4dad
Yevhenii Serdiuk <zheka2797@gmail.com>
2024-07-14 16:22:48 +0300

    set wallpaper script

--- hypr/scripts/wallpaper.sh
+++
@@ -1,6 +0,0 @@
-# image = "/media/data/Wallpapers/922100.jpg"
-# image = "/media/data/Wallpapers/art/here_i_am_waiting_by_chenbo-d6qx0s4.jpg"
-image="/media/data/Wallpapers/art/here_i_am_waiting_by_chenbo-d6qx0s4.jpg"
-echo $image
-echo "$image"
-swaybg -i "$image"
